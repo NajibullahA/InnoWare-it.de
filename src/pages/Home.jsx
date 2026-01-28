4259f5feb3c9a032d724d63391e83483e3eef006
Najibullah  Abassi <najib@Najibullahs-MacBook-Pro.local>
2026-01-28 23:11:47 +0100

    Home no content just sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div style={{ height: '100vh', background: 'red', color: 'white' }}>
-      <h1>HOME IS WORKING</h1>
+    <div>
+      <section style={{ height: '100vh', padding: '100px' }}>
+        <h1>Hero Section</h1>
+      </section>
+
+      <section id="solutions" style={{ padding: '100px' }}>
+        <h2>Solutions</h2>
+      </section>
+
+      <section id="creative" style={{ padding: '100px' }}>
+        <h2>Creative</h2>
+      </section>
+
+      <section id="contact" style={{ padding: '100px' }}>
+        <h2>Contact</h2>
+      </section>
     </div>
   );
 };
