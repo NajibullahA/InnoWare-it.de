95db4cf534e4edd06ba801329a9414b4cfb5c097
Najibullah  Abassi <najib@Najibullahs-MacBook-Pro.local>
2026-01-28 22:58:26 +0100

    Temporarily Home 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
-return (
-  <div style={{ padding: '100px', background: 'red', color: 'white' }}>
-    <h1>HOME IS RENDERING</h1>
-  </div>
-);
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ padding: '100px', background: 'red', color: 'white' }}>
+      <h1>HOME IS RENDERING</h1>
+    </div>
+  );
+};
+
+export default Home;
