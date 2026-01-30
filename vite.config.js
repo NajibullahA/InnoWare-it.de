f1076f06b029f39648a983f25e1c4af118cc5d6f
Najibullah  Abassi <najib@MacBookPro.localdomain>
2026-01-30 18:25:38 +0100

    Fix Vite base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/InnoWare-it.de/',
   plugins: [react()],
-  base: '/',
+  base: '/'
 })
